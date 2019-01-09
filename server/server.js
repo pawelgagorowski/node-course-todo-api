12a65502fea10243ce0dd7295fae41c9e01d35fc
pawel <pawelgagorowski026@gmail.com>
2019-01-09 23:11:12 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -145,7 +145,19 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user)
 })
 
+app.post('/users/login', (req, res) => {
+  var body =_.pick(req.body, ['email', 'password'])
 
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken()
+    .then((result) => {
+      return res.header('x-auth', result.token).send(user);
+    })
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+})
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`)
